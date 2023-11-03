6daaff5bb0391695d57fd71d9b5524b73cc38412
Berkay Anduv <sygrok@hotmail.com>
2023-11-04 02:42:16 +0300

    OptimisticUpdating

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { fetchEvent, updateEvent } from "../../util/http.js";
+import { fetchEvent, queryClient, updateEvent } from "../../util/http.js";
 import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
@@ -20,6 +20,21 @@ export default function EditEvent() {
   //Update/Edit
   const { mutate } = useMutation({
     mutationFn: updateEvent,
+    onMutate: async (data) => {
+      //onMutate will execute after the mutate in handleSubmit executed
+      const newEvent = data.event;
+      await queryClient.cancelQueries({ queryKey: ["events", id] }); // We added this to not clash with updating
+      previousEvent = queryClient.getQueryData(["events", id]);
+      queryClient.setQueryData(["events", id], newEvent); // it will be manipulated without waiting for the response
+
+      return { previousEvent };
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(["events", id], context.previousEvent); //When error occurs we get previous data from previousEvent to fill input with previous data
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(["events", id]); // to get latest data from the backend just in case
+    },
   });
 
   function handleSubmit(formData) {
